e43e80300501ebf8390eae71590d3f46d28e7a47
stassmoke <stanislav.kulunych@gmail.com>
2021-07-20 15:58:02 +0300

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+function setInnerHeight() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+
+window.addEventListener('resize', () => {
+    setInnerHeight()
+});
+
+setInnerHeight()
+
